1b591a44e522f2094e9b1a016125573df0f5e78e
slp <339403375@qq.com>
2018-09-27 10:53:48 +0800

    test0927

--- try.py
+++ try.py
@@ -66,6 +66,7 @@ def activationcode2sql():
     lines = f.readlines()#读取全部内容
     for line in lines:
         print(line)
+        print("hahaha")
 
 if __name__ == '__main__':
     activationcode2sql()
